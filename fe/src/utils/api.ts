05b8ad4a8bbc305ef9b6b60909ea1492cbf731ff
nathangalung <18222130@std.stei.itb.ac.id>
2025-02-02 02:22:32 +0700

    update domain

--- fe/src/utils/api.ts
+++ fe/src/utils/api.ts
@@ -29,6 +29,7 @@ async function makeRequest<T>(endpoint: string, options?: RequestInit): Promise<
     const token = localStorage.getItem('token')
     const response = await fetch(`${API_URL}${endpoint}`, {
       ...options,
+      credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': token ? `Bearer ${token}` : '',
